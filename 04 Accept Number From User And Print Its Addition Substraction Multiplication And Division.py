3a8435bdd6b181fbf985ac16d0e73ae05255e7f5
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-27 00:58:51 +0530

    Add files via upload

---
+++ 04 Accept Number From User And Print Its Addition Substraction Multiplication And Division.py
@@ -0,0 +1,14 @@
+n1 = int(input("Enter First Number For Calculation : "))
+n2 = int(input("Enter Second Number For Calculation : "))
+
+Sum = n1 + n2
+Sub = n1 - n2
+Mul = n1 * n2
+Div = n1 / n2
+mod = n1 % n2
+
+print("Summation Of Two Numbers Is : ",Sum)
+print("Substraction Of Two Numbers Is : ",Sub)
+print("Multiplication Of Two Numbers Is : ",Mul)
+print("Division Of Two Numbers Is : ",Div)
+print("Module Division Of Two Numbers Is : ",mod)
