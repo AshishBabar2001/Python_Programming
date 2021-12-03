8d86d725003ea7781dac897f3b6bed6b7aa1ebea
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-04 01:03:34 +0530

    Add files via upload

---
+++ 17 Accept Two Number From User And Print In Both Which One Is Max or Equal.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter First Number : "))
+n2 = int(input("Enter Second Number : "))
+if(n1>n2):
+    print("Max Is n1: ",n1)
+elif(n1<n2):
+    print("Max Is n2 : ",n2)
+else:
+    print("Both Number Are Equal")
