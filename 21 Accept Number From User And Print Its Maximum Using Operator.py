7764097f62770c8d3a0507749c464f0000354b98
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-06 00:46:42 +0530

    Add files via upload

---
+++ 21 Accept Number From User And Print Its Maximum Using Operator.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter First Number : "))
+n2 = int(input("Enter Second Number : "))
+n3 = int(input("Enter Third Number : "))
+
+if(n1 > n2 and n1 > n3):
+        print("Max Is n1 : ",n1)
+
+if(n2 > n1 and n2 > n3):
+        print("Max Is n2 : ",n2)
+
+if(n3 > n1 and n3 > n2):
+        print("Max Is n3 : ",n3)
