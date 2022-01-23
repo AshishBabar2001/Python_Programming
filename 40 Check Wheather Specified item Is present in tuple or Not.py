94136190b9d35fc0aaafb6a0394c21dd9e80bc9c
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-01-24 01:05:12 +0530

    Add files via upload

---
+++ 40 Check Wheather Specified item Is present in tuple or Not.py
@@ -0,0 +1,7 @@
+Stud = ("Swetali","Prafulla","Pratiksha","Sandhya")
+
+print(Stud)
+if "Prafulla" in Stud:
+    print("Prafulla Is Present in the Tuple")
+else:
+    print("Prafulla Is Not Present In Tuple")
