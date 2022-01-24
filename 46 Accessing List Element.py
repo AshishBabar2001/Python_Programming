ba45cfaba5004a497b5f30a7b1d7c2175f996ea3
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-01-25 00:25:00 +0530

    Add files via upload

---
+++ 46 Accessing List Element.py
@@ -0,0 +1,7 @@
+students = ["Ashish","Amit","Rajesh","Rahat"]
+
+print(students)
+
+print("First Element : ",students[0])
+print("Second Element : ",students[1])
+print("List Elements[0:3] : ",students[0:4])
