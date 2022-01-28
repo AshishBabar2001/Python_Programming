756131958875d699f2b3c3b85529a093bec83b79
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-01-28 12:32:05 +0530

    Add files via upload

---
+++ 54 Accept Name From User And Check Name Is Present Or Not.py
@@ -0,0 +1,10 @@
+Employee = ["Mandar","Mayuri","Aniket","Prashat","Viraj"]
+
+print(Employee)
+
+ele = input("Enter String To Search : ")
+
+if ele in Employee:
+    print(ele,"Is Present In List")
+else:
+    print(ele,"Is NOt Present In List")
