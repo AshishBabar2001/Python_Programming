5a63f40f1235320d02aec2d2b61127261010f7fc
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-02-10 03:08:51 +0530

    Add files via upload

---
+++ 59 Values Can be Accessing Using Keys of Dictionary.py
@@ -0,0 +1,8 @@
+Student = {"Name":"Rahul","Age":20,"College":"Tssm"}
+
+print(Student)
+print("Accessing Values Using Keys !!!")
+
+print("Name Is : ",Student["Name"])
+print("Age Is : ",Student["Age"])
+print("College Is : ",Student["College"])
