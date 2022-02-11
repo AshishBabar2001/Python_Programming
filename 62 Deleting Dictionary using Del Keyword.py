beab23eeea9a9737966963c4739bc67c97d1d65c
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-02-12 00:07:34 +0530

    Add files via upload

---
+++ 62 Deleting Dictionary using Del Keyword.py
@@ -0,0 +1,14 @@
+Emp = {"Name":"Ayush","Age":20,"Company":"Accenture","Salary":400000}
+
+print(Emp)
+
+del Emp["Name"]
+del Emp["Age"]
+del Emp["Company"]
+del Emp["Salary"]
+
+print("Deleting Dictionary....")
+
+print(Emp)
+
+print("Dictionary Deleted...")
